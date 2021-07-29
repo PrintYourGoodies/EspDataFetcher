50475a80f67d3873375677840921239decdf8859
Mateusz <mateuszkiryluk@gmail.com>
2021-07-29 19:34:01 +0200

    Standaryzacja do jsona

--- src/main/java/com/mkyong/web/controller/HelloController.java
+++ src/main/java/com/mkyong/web/controller/HelloController.java
@@ -1,12 +1,24 @@
 package com.mkyong.web.controller;
 
+import com.mkyong.web.controller.dbUtils.dbUtils;
+import org.h2.util.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URLDecoder;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+
+import static javax.print.attribute.standard.ReferenceUriSchemesSupported.HTTP;
+
 
 @Controller
 public class HelloController {
@@ -25,17 +37,51 @@ public class HelloController {
         ModelAndView model = new ModelAndView();
         model.setViewName("hello");
         model.addObject("msg", name);
-        System.out.println("Time:"+ LocalDateTime.now()+"\tValue recived: " + name);
+        System.out.println("Time:" + LocalDateTime.now() + "\tValue recived: " + name);
         return model;
 
     }
+
     //http://192.168.1.29:8080/spring3/dataCollector?temp=100&hum=15
     @RequestMapping(value = "/dataCollector", method = RequestMethod.GET)
     @ResponseBody
     public String getWeatherConditionsFromRequestParams(
-            @RequestParam("hum") float hum,@RequestParam("temp") float temp) {
-        LocalDateTime roundFloor =  LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
-        System.out.println("Time:"+ roundFloor+"\tValue recived: Temp= "+temp+" C Humidity= "+hum);
+            @RequestParam("hum") float hum, @RequestParam("temp") float temp) {
+        LocalDateTime roundFloor = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+        dbUtils.test(hum, temp);
+        System.out.println("Time:" + roundFloor + "\tValue recived: Temp= " + temp + " C Humidity= " + hum);
+        return "";
+    }
+
+    @RequestMapping(value = "/dataFetcher", method = RequestMethod.GET)
+    @ResponseBody
+    public String getJson(  @RequestParam("json") String json) {
+        HttpServletRequest request =
+                ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+                        .getRequest();
+        System.out.println(json);
+        return "";
+    }
+    @RequestMapping(value = "/postJson", method = RequestMethod.POST)
+    @ResponseBody
+    public String getJson( HttpServletRequest request,
+                           HttpServletResponse response) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    request.getInputStream()));
+            String line = null;
+            StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            String reqBody = URLDecoder.decode(sb.toString());
+            System.out.println("Request Body:" + reqBody);
+//            JSONObject json = new JSONObject(reqBody);
+       int i=0;
+        } catch (Exception e) {
+            return null;
+        }
         return "";
     }
 }
---
+++ src/main/java/com/mkyong/web/controller/dbUtils/dbUtils.java
@@ -0,0 +1,35 @@
+package com.mkyong.web.controller.dbUtils;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class dbUtils {
+
+    static String url = "jdbc:postgresql://192.168.1.120:5432/smartHome";
+    static String user = "mkiryluk";
+    static String password = "qwerty";
+
+    public static void test(float current, float power){
+
+        String query = "INSERT INTO power_measurement(current, power) VALUES(?, ?)";
+
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             PreparedStatement pst = con.prepareStatement(query)) {
+
+            pst.setFloat(1, current);
+            pst.setFloat(2, power);
+            pst.executeUpdate();
+
+        } catch (SQLException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+    }
+}
+
+
